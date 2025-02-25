c240f0ebef155e2d7feb2b161486f02b184157a4
pental <pental@kakao.com>
2025-02-25 14:25:22 +0900

    [Silver III] Title: 염색체, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9342. 염색체/염색체.py
@@ -0,0 +1,31 @@
+# 백준 9342 - 염색체
+# 분류 : 문자열
+
+T = int(input())
+for _ in range(T):
+    S = input()
+    if len(S) < 3 :
+        print("Good")
+        continue
+    
+    if S[0] != "A" :
+        if S[0] not in ["A", "B", "C", "D", "E", "F"] :
+            print("Good")
+            continue
+        S = S[1:]
+    
+    if S[-1] != 'C' :
+        if S[-1] not in ["A", "B", "C", "D", "E", "F"] :
+            print("Good")
+            continue
+        S = S[:-1]
+    
+    T = ""
+    for i in range(len(S)) :
+        if i == 0 or S[i] != S[i - 1] :
+            T += S[i]
+    
+    if T == "AFC" :
+        print("Infected!")
+    else :
+        print("Good")
