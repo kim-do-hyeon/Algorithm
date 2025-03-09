193120d96c0793df0f6da581434f3ee071a762d6
pental <pental@kakao.com>
2025-03-10 01:14:26 +0900

    [Gold V] Title: 행복 유치원, Time: 260 ms, Memory: 66292 KB -BaekjoonHub

---
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.py
@@ -0,0 +1,13 @@
+# 백준 13164 - 행복 유치원
+# 분류 : 그리디
+
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+D = [A[i + 1] - A[i] for i in range(N - 1)]
+D.sort(reverse=True)
+
+answer = A[N - 1] - A[0]
+for i in range(K - 1) :
+    answer -= D[i]
+
+print(answer)
