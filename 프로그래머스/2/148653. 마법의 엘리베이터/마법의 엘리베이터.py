22e2a1d58aa27c1dd779782aa8332a5d280f35af
pental <pental@kakao.com>
2025-03-29 03:11:12 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -0,0 +1,14 @@
+def solution(storey):
+    answer = 0
+    
+    while storey > 0 :
+        digit = storey % 10
+        next_digit = (storey // 10) % 10
+        if digit > 5 or (digit == 5 and next_digit >= 5) :
+            answer += 10 - digit
+            storey += 10
+        else :
+            answer += digit
+        
+        storey //= 10
+    return answer
