a4963551693889c26283630ae87d1a25cc69fcb7
pental <pental@kakao.com>
2025-04-19 14:38:37 +0900

    [Bronze I] Title: 부재중 전화, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/1333. 부재중 전화/부재중 전화.py
@@ -0,0 +1,19 @@
+# 백준 1333 - 부재중 전화
+# 분류 : 구현
+
+N, L, D = map(int, input().split())
+
+check = [False] * 5000
+
+index = 0
+for _ in range(N) :
+    for _ in range(L) :
+        check[index] = True
+        index += 1
+    for _ in range(5) :
+        index += 1
+
+for i in range(0, 5000, D) :
+    if not check[i] :
+        print(i)
+        break
