0e8c21ec78c11fe59928cee7a908c72429878351
pental <pental@kakao.com>
2025-05-04 13:41:06 +0900

    [Bronze I] Title: 관광, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10041. 관광/관광.py
@@ -0,0 +1,22 @@
+# 백준 10041 - 관광
+# 분류 : 수학
+
+W, H, N = map(int, input().split())
+P = [list(map(int, input().split())) for _ in range(N)]
+
+answer = 0
+for i in range(N - 1) :
+    # i - > i + 1
+    x1, y1 = P[i]
+    x2, y2 = P[i + 1]
+
+    if x1 > x2 :
+        x1, x2 = x2, x1
+        y1, y2 = y2, y1
+    
+    if y1 > y2 :
+        answer += abs(x1 - x2) + abs(y1 - y2)
+    else :
+        answer += max(abs(x1 - x2), abs(y1 - y2))
+
+print(answer)
