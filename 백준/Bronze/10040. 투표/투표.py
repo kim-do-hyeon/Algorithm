2c244c8de82184f72fd7332b3ff1955a95b869f5
pental <pental@kakao.com>
2025-05-04 12:32:54 +0900

    [Bronze II] Title: 투표, Time: 148 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10040. 투표/투표.py
@@ -0,0 +1,23 @@
+# 백준 10040 - 투표
+# 분류 : 구현
+
+N, M = map(int, input().split())
+A = [int(input()) for _ in range(N)]
+B = [int(input()) for _ in range(M)]
+
+count = [0] * N
+
+for i in range(M) :
+    for j in range(N) :
+        if A[j] <= B[i] :
+            count[j] += 1
+            break
+
+max_vote = 0
+who = -1
+for i in range(N) :
+    if max_vote < count[i] :
+        max_vote = count[i]
+        who = i
+
+print(who + 1)
