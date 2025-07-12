db2fc6e2e00b99a40f1a9f15308ed82b8be465a9
pental <pental@kakao.com>
2025-07-12 12:32:53 +0900

    [Gold V] Title: 간선 이어가기 2, Time: 336 ms, Memory: 59084 KB -BaekjoonHub

---
+++ 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.py
@@ -0,0 +1,39 @@
+# 백준 14284 - 간선 이어가기 2
+# 분류 : 다익스트라
+
+import sys
+from queue import PriorityQueue
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+adj = [[] for _ in range(N)]
+
+for _ in range(M) :
+    a, b, c = map(int, input().split())
+    a, b = a - 1, b - 1
+
+    adj[a].append((b, c))
+    adj[b].append((a, c))
+
+S, T = map(int, input().split())
+S, T = S - 1, T - 1
+
+dist = [1e9] * N
+pq = PriorityQueue()
+
+pq.put((0, S))
+dist[S] = 0
+
+while pq.qsize() != 0 :
+    d, u = pq.get()
+
+    if d != dist[u] :
+        continue
+    
+    for v, w in adj[u]: 
+        if dist[v] > dist[u] + w :
+            dist[v] = dist[u] + w
+            pq.put((dist[v], v))
+
+print(dist[T])
