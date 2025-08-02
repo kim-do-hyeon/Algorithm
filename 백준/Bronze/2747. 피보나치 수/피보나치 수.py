ec07b21716c35f4d6b0edf9e1c7a50a566c68b85
pental <pental@kakao.com>
2025-08-02 11:42:14 +0900

    [Bronze II] Title: 피보나치 수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -0,0 +1,9 @@
+# 백준 2747 - 피보나치 수
+def dp_fibo(n):
+    dp = [0] * (n+1)
+    dp[0], dp[1] = 0, 1
+    for i in range(2, n+1):
+        dp[i] = dp[i-2] + dp[i-1]
+    return dp[n]
+
+print(dp_fibo(int(input())))
