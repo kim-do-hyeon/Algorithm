9bf11f97e303acb0cfc1d6fe54406e6e3de70869
pental <pental@kakao.com>
2025-12-10 13:42:53 +0900

    [Bronze III] Title: 사나운 개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2991. 사나운 개/사나운 개.py
@@ -0,0 +1,10 @@
+# [브론즈 3] 백준 2991 - 사나운 개
+A, B, C, D = map(int, input().split())
+li = list(map(int, input().split()))
+for n in li:
+    attacked = 0
+    if 0 < n % (A+B) <= A:
+        attacked += 1
+    if 0 < n % (C+D) <= C:
+        attacked += 1
+    print(attacked)
